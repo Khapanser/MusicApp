6b0f7d05b6bec56749d86fdc00288ea7826cff94
Khapanser <khapanser@gmail.com>
2018-01-26 01:38:09 +0300

    test gui
    Signed-off-by: khapanser <Khapanser@gmail.com>

---
+++ src/main/java/FrameTest.java
@@ -0,0 +1,17 @@
+package main.java;
+
+import  javax.swing.*;
+
+public class FrameTest {
+    public static void main (String[] args)
+    {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click me!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+
+    }
+
+}
