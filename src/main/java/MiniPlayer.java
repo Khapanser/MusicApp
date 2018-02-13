a45a19481d2c8e6f2e245a7f4ccd85add0c8cf92
khapanser <khapanser@gmail.com>
2018-02-13 23:39:53 +0300

    controller2
    Signed-off-by: khapanser <Khapanser@gmail.com>

--- src/main/java/MiniPlayer.java
+++ src/main/java/MiniPlayer.java
@@ -2,9 +2,24 @@ package main.java;
 
 
 import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
 
-public class MiniPlayer /*implements ControllerEventListener*/ {
+public class MiniPlayer {
+
+    static JFrame f;
+    static MyPanel control;
+
+    public void go(){
+        f = new JFrame("Music plus Graphics");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(400, 400);
+        f.getContentPane().add(control);
+        f.setVisible(true);
+    }
+
     public static void main(String[] args)
+            go()
     {
         try{
 
@@ -12,7 +27,7 @@ public class MiniPlayer /*implements ControllerEventListener*/ {
             sequencer.open();
 
             int[] eventsIWant = {127};
-            Control control = new Control();
+            control = new MyPanel();
             sequencer.addControllerEventListener(control,eventsIWant);
 
 
@@ -41,18 +56,27 @@ public class MiniPlayer /*implements ControllerEventListener*/ {
                 return event;
     }
 
- /*   public void controlChange (ShortMessage event)
-    {
-        System.out.println("la");
-    }*/
-
-
+    /*
     static class  Control implements ControllerEventListener {
         public void controlChange(ShortMessage event)
         {
             System.out.println(" LA ");
         }
     }
+    */
 
+    static class MyPanel extends JPanel implements ControllerEventListener{
+        public void controlChange(ShortMessage event)
+        {
+            System.out.println(" LA ");
+            repaint();
+        }
+
+        public void PaintComponent(Graphics g)
+        {
+
+        }
+
+    }
 
 }
