942709db563f77dcbb708e6874efe79ed5d1f94a
khapanser <khapanser@gmail.com>
2018-03-04 22:39:16 +0300

    scrollerAdded
    Signed-off-by: khapanser <Khapanser@gmail.com>

--- src/main/java/Organizer.java
+++ src/main/java/Organizer.java
@@ -6,7 +6,7 @@ import java.awt.*;
 
 public class Organizer {
     JFrame frame;
-    //JButton b1;
+    JButton b1;
     JPanel panel1;
     JPanel panel2;
     JPanel panel3;
@@ -21,19 +21,37 @@ public class Organizer {
     }
 
     public void go(){
-        String[] str = {"First","Second","Third"};
+        String[] str = {"UGCS-1142 - Description ","UGCS-1515 - Description2","UGCS-3893 - Description3", "UGCS-2900 - Description4",
+                        "UGCS-1414 - Description5", "UGCS-1434 - Description6"};
         panel3 = new JPanel();
         frame = new JFrame("Organizer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel3.setLayout(new BoxLayout(panel3,BoxLayout.X_AXIS));
 
 
-        panel2 = new JPanel();
         list = new JList<String>(str);
-        panel2.add(list);
+
+        //Add scroll
+        JScrollPane scroller = new JScrollPane(list);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setVisibleRowCount(4);
+
+
+        b1 = new JButton("SELECT");
+        panel2 = new JPanel();
+        panel2.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        panel2.setLayout(new BoxLayout(panel2,BoxLayout.Y_AXIS));
+        //panel2.add(BorderLayout.NORTH,list);
+        //panel2.add(BorderLayout.SOUTH, b1);
+        //panel2.add(list);
+        panel2.add(scroller);
+        panel2.add(b1);
 
 
         panel1 = new JPanel();
+
         editor1 = new JEditorPane();
         editor1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         editor2 = new JEditorPane();
@@ -53,6 +71,7 @@ public class Organizer {
         frame.setSize(500, 500);
         frame.setVisible(true);
         frame.add(menu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         
     }
